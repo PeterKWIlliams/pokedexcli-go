2f469f5ee557d8e4aaf81ef1be5a88adcb658ff0
Peter Williams <peterkennethwilliams@gmail.com>
2024-08-13 20:40:07 +0100

    Implement main function
    This commit sets up the main entry point for the application. It creates
    a new API client with a 10-second timeout, initializes a cache with a
    5-second timeout, configures the client for the REPL, and starts the
    REPL.

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"time"
+
+	"github.com/PeterKWIlliams/pokedexcli-go/internal/commands"
+	"github.com/PeterKWIlliams/pokedexcli-go/internal/pokecache"
+	"github.com/PeterKWIlliams/pokedexcli-go/internal/repl"
+	"github.com/PeterKWIlliams/pokedexcli-go/internal/thirdparty/api"
 )
 
 func main() {
-	fmt.Println("hello world")
+	client := api.NewClient(time.Second * 10)
+	pokecache.NewCache(time.Second * 5)
+	cfg := &commands.Config{
+		Client: client,
+	}
+	repl.Start(cfg)
 }
